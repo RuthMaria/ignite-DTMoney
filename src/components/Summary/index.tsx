8b49af82010ef263418f67efabef8504832ddbb6
RuthMaria <ruth-15rms@hotmail.com>
2022-12-20 18:22:27 -0300

    feat: add useTheme hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useTheme } from 'styled-components'; // usado para aproveitar o tema da aplicação fora do styled-component, para poder ser usado nos componentes
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 import { SummaryCard, SummaryContainer } from './styles';
 
 export const Summary: React.FC = () => {
+  const theme = useTheme();
+
   return (
     <SummaryContainer>
       <SummaryCard>
         <header>
           <span>Entradas</span>
-          <ArrowCircleUp size={32} color="#00b37e" />
+          <ArrowCircleUp size={32} color={theme['green-300']} />
         </header>
 
         <strong>R$ 17.400,00</strong>
@@ -17,7 +20,7 @@ export const Summary: React.FC = () => {
       <SummaryCard>
         <header>
           <span>Saídas</span>
-          <ArrowCircleDown size={32} color="#f75a68" />
+          <ArrowCircleDown size={32} color={theme['red-300']} />
         </header>
 
         <strong>R$ 17.400,00</strong>
@@ -26,7 +29,7 @@ export const Summary: React.FC = () => {
       <SummaryCard variant="green">
         <header>
           <span>Total</span>
-          <CurrencyDollar size={32} color="#fff" />
+          <CurrencyDollar size={32} color={theme.white} />
         </header>
 
         <strong>R$ 17.400,00</strong>
