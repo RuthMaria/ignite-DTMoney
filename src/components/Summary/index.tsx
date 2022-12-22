5d166c8173982d229d577eed1d95e0411646ae19
RuthMaria <ruth-15rms@hotmail.com>
2022-12-21 22:26:51 -0300

    feat: calculating transaction summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -7,8 +7,26 @@ import { SummaryCard, SummaryContainer } from './styles';
 
 export const Summary: React.FC = () => {
   const { transactions } = useContext(TransactionsContext);
-
   const theme = useTheme();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
 
   return (
     <SummaryContainer>
@@ -18,7 +36,7 @@ export const Summary: React.FC = () => {
           <ArrowCircleUp size={32} color={theme['green-300']} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -27,7 +45,7 @@ export const Summary: React.FC = () => {
           <ArrowCircleDown size={32} color={theme['red-300']} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -36,7 +54,7 @@ export const Summary: React.FC = () => {
           <CurrencyDollar size={32} color={theme.white} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
